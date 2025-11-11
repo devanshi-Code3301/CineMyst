66fa5dee690dc9b3aaf61c8fae95d6f61c7d0025
zydnet <devanshi.ag1@gmail.com>
2025-11-11 08:09:18 +0530

    Initial Commit

---
+++ CineMystApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineMystApp
+//
+//  Created by user@50 on 11/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
